aa3e616c1bbe3f17e52a46614b7ed9a552fbfb0d
Victor Oliveira <vamo89@gmail.com>
2017-05-17 23:03:37 -0300

    Creating matrix and drawing based on that

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const ctx = document.getElementById('tetris-canvas').getContext('2d')
+const tetris = require('./lib/tetris')
 
+const ctx = document.getElementById('tetris-canvas').getContext('2d')
+ctx.scale(20, 20)
 ctx.fillStyle = 'red'
-ctx.fillRect(20, 20, 100, 100)
+
+const maxWidth = 12
+const maxHeight = 20
+const matrix = tetris.clearMatrix(maxWidth, maxHeight)
+
+matrix[2][1] = 1
+
+draw(ctx, matrix)
+
+function draw (ctx, matrix) {
+  matrix.forEach((row, y) =>
+    row.forEach((value, x) => {
+      if (value) { ctx.fillRect(x, y, 1, 1) }
+    })
+  )
+}
