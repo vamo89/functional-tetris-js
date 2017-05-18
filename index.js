a0a6021c71aa0d0d440eb77813985289a5d1c055
Victor Oliveira <vamo89@gmail.com>
2017-05-17 23:04:47 -0300

    Drawing all possible tetris-forms on matrix

--- index.js
+++ index.js
@@ -8,7 +8,47 @@ const maxWidth = 12
 const maxHeight = 20
 const matrix = tetris.clearMatrix(maxWidth, maxHeight)
 
+// Create T piece
 matrix[2][1] = 1
+matrix[2][2] = 1
+matrix[2][3] = 1
+matrix[3][2] = 1
+
+// Create L piece
+matrix[5][5] = 1
+matrix[6][5] = 1
+matrix[7][5] = 1
+matrix[7][6] = 1
+
+// Create inverted-L piece
+matrix[12][2] = 1
+matrix[13][2] = 1
+matrix[14][2] = 1
+matrix[14][1] = 1
+
+// Create I piece
+matrix[1][8] = 1
+matrix[2][8] = 1
+matrix[3][8] = 1
+matrix[4][8] = 1
+
+// Create O piece
+matrix[8][8] = 1
+matrix[9][8] = 1
+matrix[8][9] = 1
+matrix[9][9] = 1
+
+// Create Z piece
+matrix[15][5] = 1
+matrix[15][6] = 1
+matrix[16][6] = 1
+matrix[16][7] = 1
+
+// Create inverted-Z piece
+matrix[12][5] = 1
+matrix[12][6] = 1
+matrix[13][5] = 1
+matrix[13][4] = 1
 
 draw(ctx, matrix)
 
