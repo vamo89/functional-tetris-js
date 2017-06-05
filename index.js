9ab8b3277a4bab8c7f4ec6e4d53ec4fdf575a423
Victor Oliveira <vamo89@gmail.com>
2017-06-05 14:49:32 -0300

    Function to create new piece at matrix

--- index.js
+++ index.js
@@ -2,60 +2,30 @@ const tetris = require('./lib/tetris')
 
 const ctx = document.getElementById('tetris-canvas').getContext('2d')
 ctx.scale(20, 20)
-ctx.fillStyle = 'red'
 
 const maxWidth = 12
 const maxHeight = 20
-const matrix = tetris.clearMatrix(maxWidth, maxHeight)
+const colors = tetris.colors()
 
-// Create T piece
-matrix[2][1] = 1
-matrix[2][2] = 1
-matrix[2][3] = 1
-matrix[3][2] = 1
+var matrix = tetris.clearMatrix(maxWidth, maxHeight)
 
-// Create L piece
-matrix[5][5] = 1
-matrix[6][5] = 1
-matrix[7][5] = 1
-matrix[7][6] = 1
-
-// Create inverted-L piece
-matrix[12][2] = 1
-matrix[13][2] = 1
-matrix[14][2] = 1
-matrix[14][1] = 1
-
-// Create I piece
-matrix[1][8] = 1
-matrix[2][8] = 1
-matrix[3][8] = 1
-matrix[4][8] = 1
-
-// Create O piece
-matrix[8][8] = 1
-matrix[9][8] = 1
-matrix[8][9] = 1
-matrix[9][9] = 1
-
-// Create Z piece
-matrix[15][5] = 1
-matrix[15][6] = 1
-matrix[16][6] = 1
-matrix[16][7] = 1
-
-// Create inverted-Z piece
-matrix[12][5] = 1
-matrix[12][6] = 1
-matrix[13][5] = 1
-matrix[13][4] = 1
+matrix = tetris.createPieceWithMatrix(matrix)('I', 0, 0)
+matrix = tetris.createPieceWithMatrix(matrix)('J', 1, 2)
+matrix = tetris.createPieceWithMatrix(matrix)('L', 5, 5)
+matrix = tetris.createPieceWithMatrix(matrix)('O', 9, 3)
+matrix = tetris.createPieceWithMatrix(matrix)('S', 14, 4)
+matrix = tetris.createPieceWithMatrix(matrix)('T', 15, 9)
+matrix = tetris.createPieceWithMatrix(matrix)('Z', 6, 8)
 
 draw(ctx, matrix)
 
 function draw (ctx, matrix) {
   matrix.forEach((row, y) =>
     row.forEach((value, x) => {
-      if (value) { ctx.fillRect(x, y, 1, 1) }
+      if (value) {
+        ctx.fillStyle = colors[value]
+        ctx.fillRect(x, y, 1, 1)
+      }
     })
   )
 }
--- test/tetris.test.js
+++ test/tetris.test.js
@@ -27,3 +27,94 @@ const clearMatrixTest = [
 test('Clear Matrix', (t) => {
   t.deepEqual(tetris.clearMatrix(), clearMatrixTest)
 })
+
+test('Matrix with piece dont change original matrix', (t) => {
+  var originalMatrix = tetris.clearMatrix()
+  tetris.createPieceWithMatrix(originalMatrix)('I', 0, 0)
+  t.deepEqual(originalMatrix, tetris.clearMatrix())
+})
+
+test('Matrix with I at 0,0', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('I', 0, 0)
+  var matrixToTestAgainst = tetris.clearMatrix()
+  matrixToTestAgainst[0][0] = 1
+  matrixToTestAgainst[1][0] = 1
+  matrixToTestAgainst[2][0] = 1
+  matrixToTestAgainst[3][0] = 1
+  t.deepEqual(matrixToTestAgainst, matrix)
+})
+
+test('Matrix with J at 0,0', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('J', 0, 0)
+  var matrixToTestAgainst = tetris.clearMatrix()
+  matrixToTestAgainst[0][1] = 2
+  matrixToTestAgainst[1][1] = 2
+  matrixToTestAgainst[2][1] = 2
+  matrixToTestAgainst[2][0] = 2
+  t.deepEqual(matrixToTestAgainst, matrix)
+})
+
+test('Matrix with L at 0,0', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('L', 0, 0)
+  var matrixToTestAgainst = tetris.clearMatrix()
+  matrixToTestAgainst[0][0] = 3
+  matrixToTestAgainst[1][0] = 3
+  matrixToTestAgainst[2][0] = 3
+  matrixToTestAgainst[2][1] = 3
+  t.deepEqual(matrixToTestAgainst, matrix)
+})
+
+test('Matrix with O at 0,0', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('O', 0, 0)
+  var matrixToTestAgainst = tetris.clearMatrix()
+  matrixToTestAgainst[0][0] = 4
+  matrixToTestAgainst[1][0] = 4
+  matrixToTestAgainst[0][1] = 4
+  matrixToTestAgainst[1][1] = 4
+  t.deepEqual(matrixToTestAgainst, matrix)
+})
+
+test('Matrix with S at 0,0', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('S', 0, 0)
+  var matrixToTestAgainst = tetris.clearMatrix()
+  matrixToTestAgainst[0][1] = 5
+  matrixToTestAgainst[0][2] = 5
+  matrixToTestAgainst[1][1] = 5
+  matrixToTestAgainst[1][0] = 5
+  t.deepEqual(matrixToTestAgainst, matrix)
+})
+
+test('Matrix with T at 0,0', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('T', 0, 0)
+  var matrixToTestAgainst = tetris.clearMatrix()
+  matrixToTestAgainst[0][0] = 6
+  matrixToTestAgainst[0][1] = 6
+  matrixToTestAgainst[0][2] = 6
+  matrixToTestAgainst[1][1] = 6
+  t.deepEqual(matrixToTestAgainst, matrix)
+})
+
+test('Matrix with Z at 0,0', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('Z', 0, 0)
+  var matrixToTestAgainst = tetris.clearMatrix()
+  matrixToTestAgainst[0][0] = 7
+  matrixToTestAgainst[0][1] = 7
+  matrixToTestAgainst[1][1] = 7
+  matrixToTestAgainst[1][2] = 7
+  t.deepEqual(matrixToTestAgainst, matrix)
+})
+
+test('Matrix with piece outside 0,0', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('I', 5, 6)
+  var matrixToTestAgainst = tetris.clearMatrix()
+  matrixToTestAgainst[5][6] = 1
+  matrixToTestAgainst[6][6] = 1
+  matrixToTestAgainst[7][6] = 1
+  matrixToTestAgainst[8][6] = 1
+  t.deepEqual(matrixToTestAgainst, matrix)
+})
+
+test('Matrix with unknow piece', (t) => {
+  var matrix = tetris.createPieceWithMatrix(tetris.clearMatrix())('M', 0, 0)
+  t.deepEqual(tetris.clearMatrix(), matrix)
+})
