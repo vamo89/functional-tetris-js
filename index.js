f25cefdcd7de6d40260e1c1e85cef66bae2b6176
Victor Oliveira <vamo89@gmail.com>
2017-07-06 17:51:24 -0300

    Draft to start game actions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const tetris = require('./lib/tetris')
 
 const ctx = document.getElementById('tetris-canvas').getContext('2d')
 ctx.scale(20, 20)
+ctx.font = '1px times'
 
 const maxWidth = 12
 const maxHeight = 20
@@ -11,6 +12,7 @@ let matrix = tetris.clearMatrix(maxWidth, maxHeight)
 let lastTime = 0
 let dropCounter = 0
 const dropInterval = 1000
+let playerPoints = 0
 update()
 
 function draw (ctx, matrix) {
@@ -24,16 +26,34 @@ function draw (ctx, matrix) {
   )
 }
 
+function drawPoints (ctx, playerPoints) {
+  ctx.fillStyle = 'black'
+  ctx.fillText('Points: ' + playerPoints, 0, maxHeight)
+}
+
 function update (time = 0) {
   dropCounter += time - lastTime
   lastTime = time
   if (dropCounter > dropInterval) {
     dropCounter = 0
-    matrix = tetris.createNewPiece(matrix, randomPiece())
+    gameTick()
+    ctx.clearRect(0, 0, maxWidth, maxHeight)
     draw(ctx, matrix)
+    drawPoints(ctx, playerPoints)
   }
 
   window.requestAnimationFrame(update)
+}
+
+function gameTick () {
+  if (tetris.checkLineComplete(matrix)) {
+    playerPoints += tetris.calculatePointsForLineCompletion(matrix)
+    matrix = tetris.removeCompleteLines(matrix)
+  } else if (tetris.checkFloatingPiece(matrix)) {
+    matrix = tetris.gravityWork(matrix)
+  } else {
+    matrix = tetris.createNewPiece(matrix, randomPiece())
+  }
 }
 
 function randomPiece () {
